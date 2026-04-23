ba6b54f642119544e5ac2ea16004d6bd164bcdf0
Tran hong <tranhong2411@gmail.com>
2026-04-23 11:33:15 +0700

    Update the first comment

---
+++ bai1.py
@@ -0,0 +1,26 @@
+
+a1 = 100
+name =  "tran hong" 
+
+print(name)
+print(a1)
+
+gio_hang = a1+50
+if gio_hang>100:
+    thanh_toan = gio_hang -gio_hang*0.1
+else:
+    thanh_toan = gio_hang
+
+
+print("gio hang co gia tri : " , gio_hang)
+print("Ten cua ban la : ", name)
+print("tong_tien:", thanh_toan)
+
+x = 3
+ 
+if x > 10 : 
+    print("lon hon 10")
+elif x > 5: 
+    print("tu 6 den 10")
+else :
+    print("5 hoac nho hon")
---
+++ homework1.py
@@ -0,0 +1,7 @@
+bai1
+
+prince =120000
+quantily = 3
+
+Tong_tien_hang =prince*quantily
+print("Tổng tiền :"Tong_tien_hang VND)
